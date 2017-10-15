25f8b6473a7fe8096ce01109419be94ee1d9bc73
lucas <lucasjoao.lj@gmail.com>
2017-10-15 20:51:07 -0200

    text -> number; work in view with session

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, url_for, redirect
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
 @app.route("/")
@@ -15,4 +16,17 @@ def doc():
 
 @app.route("/config", methods=['GET', 'POST'])
 def config():
+    if request.method == 'POST':
+        # TODO: objeto simulador
+        # TODO: secret key
+        # TODO: realizar um teste de como fazer para pause
+        # TODO: algoritmo em si
+        session['simulator'] = request.form
+        return redirect(url_for('simulate'))
     return render_template('config.html')
+
+
+@app.route("/simulate")
+def simulate():
+    print(session['simulator'])
+    return render_template('simulate.html')
